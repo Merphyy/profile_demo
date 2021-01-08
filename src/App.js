880b0a70829ff96462d1a02908d608629b5e2ce4
merphyy <619503233@qq.com>
2021-01-08 16:21:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // const [data, setData] = useState({});
+  // useEffect(() => {}, []);
+  const d = new Date('2015-03-25T12:00:00Z').toISOString().split('T')[0];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='card' style={{ width: '18rem', alignContent: 'center' }}>
+        <img
+          class='card-img-top'
+          src='https://media-exp1.licdn.com/dms/image/C4D03AQFw-2Ewps3iwg/profile-displayphoto-shrink_400_400/0/1563640488644?e=1615420800&v=beta&t=5U3_X81AkWpcIO4g0kg2d0-1zhdgFTO6IhSoh1Yc8Lc'
+        />
+        <div className='card-body'>
+          <h5 className='card-title'>Dennis Li</h5>
+          <p className='card-text'>CEO & Founder, Example</p>
+          <p className='card-text'>date of birth:{d}</p>
+          <a href='#' className='btn btn-primary'>
+            Contact
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
